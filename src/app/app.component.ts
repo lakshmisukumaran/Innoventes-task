2c782c4c52388a4dc8c164029c7c98450c96f5da
lakshmi <Lakshmi@bravocode.in>
2019-05-17 22:07:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'booking';
+  rooms = 1;
+  adults = 1;
+  childrens = 0;
+
+  plus(elem) {
+    if(elem === 'rooms') {
+      this.rooms += 1;
+      if(this.rooms > this.adults) {
+        this.adults += 1;
+      }
+    } else if(elem === 'adults') {
+      this.adults += 1;
+      if(this.rooms < ((this.adults + this.childrens)/4)) {
+        this.rooms += 1;
+      }
+    } else if(elem === 'childrens'){
+      this.childrens += 1;
+      if(this.rooms < ((this.adults + this.childrens)/4)) {
+        this.rooms += 1;
+      }
+    }
+    
+  }
+
+  minus(elem) {
+    if(elem === 'rooms') {
+      this.rooms -= 1;
+      if((this.rooms * 4) < (this.adults + this.childrens)) {
+
+        this.childrens -= ((this.adults + this.childrens) % (this.rooms * 4));
+        if(this.childrens < 0){
+          this.adults+=this.childrens;
+           this.childrens=0;
+        }
+        
+        else if(this.childrens == 0){
+          this.adults-=4;
+        }
+    } else if(elem === 'adults') {
+      this.adults -= 1;
+      if(((this.adults + this.childrens) % 4) == 0) {
+        this.rooms -= 1;
+      }
+     
+
+    } else if(elem === "childrens"){
+      this.childrens -= 1;
+
+      if(((this.adults + this.childrens) % 4) == 0) {
+        this.rooms -= 1;
+      }
+
+    }
+  }
 }
+}
+
